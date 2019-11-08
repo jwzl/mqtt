ce6dc4066b2fa5244c2b0008b2e42267ea8dd72e
Qing <chang.qing@advantech.com.cn>
2019-11-08 18:26:09 +0800

    Overide the mqtt

--- client/client.go
+++ client/client.go
@@ -1,5 +1,87 @@
 package client
 
 import (
+	"k8s.io/klog"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
+
+const (
+	MQTT_CLIENT_DISCONNECTED	= "Disconneted"
+	MQTT_CLIENT_CONNECTED	= "Conneted"
+)
+
+type Client struct {
+	Host			string
+	ClientID		string
+	keepAliveInterval int64
+	PingTimeout		  int64	
+
+	State 			string	
+	// tls config
+	tlsConfig 		*tls.Config
+	client  		*MQTTClient
+}
+
+func NewClient() *Client{
+	client := &Client{
+		State: MQTT_CLIENT_DISCONNECTED,
+	}
+
+	// Create mqtt client.
+	c := &MQTTClient{
+		Host:			client.Host,
+		ClientID:		client.ClientID,
+		keepAliveInterval:	client.keepAliveInterval,
+		PingTimeout:		client.PingTimeout,			
+		CleanSession:	true,
+		FileStorePath: "memory",
+		OnConnect:	client.ClientOnConnect,
+		OnLost:		client.ClientOnLost,			
+		WillTopic:		"",			//no will topic.	
+		TLSConfig:		client.tlsConfig,  
+	}
+
+	client.client = c
+
+	return client 
+}
+
+func (c *Client) Start(){
+	//Start the mqtt client
+	c.client.Start()
+
+	if err := c.client.Connect(); err != nil {
+		klog.Infof("Client connecte err (%v)", err)
+		return
+	}
+
+	klog.Infof("Client connecte Successful")
+}
+
+func (c *Client) Publish(topic string, msg *model.Message) error {
+	return c.client.Publish(topic, 2, false, msg)
+}
+
+func (c *Client) Subscribe(topic string, fn func(msg *model.Message)) error {
+	return c.client.Subscribe(topic, 2, fn)
+}
+
+func (c *Client) Unsubscribe(topics string) error {
+	return c.client.Unsubscribe(topics)
+}
+
+func (c *Client) Close(){
+	c.client.Close()
+}
+
+func (c *Client) ClientOnConnect(client mqtt.Client){
+	klog.Infof("Client (%s) connected", c.ClientID)
+
+	c.State = MQTT_CLIENT_CONNECTED
+}
+
+func (c *Client) ClientOnLost(client mqtt.Client, err error){
+	klog.Infof("Client (%s) disconnected with error (%v)", c.ClientID, err)
+
+	c.State = MQTT_CLIENT_DISCONNECTED
+}
--- client/mqtt.go
+++ client/mqtt.go
@@ -2,16 +2,17 @@ package client
 
 import (
 	"k8s.io/klog"
+	"github.com/jwzl/wssocket/translator"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 type MQTTClient struct{
 	Host			string
-	
 	ClientID		string
 	CleanSession 	bool
 	keepAliveInterval int64
 	PingTimeout		  int64	
+	// optinal as below.
 	OnConnect		mqtt.OnConnectHandler
 	OnLost			mqtt.ConnectionLostHandler
 	FileStorePath	string
@@ -26,7 +27,7 @@ type MQTTClient struct{
 }
 
 func NewMQTTClient() *MQTTClient {
-	
+	return &MQTTClient{}
 }
 
 func (mc *MQTTClient) Start() {
@@ -74,4 +75,81 @@ func (mc *MQTTClient) Connect() error {
 	}
 
 	return nil
+}
+
+//Publish
+// We Encode this model message and publishe it.
+func (mc *MQTTClient) Publish(topic string, qos byte, retained bool, msg *model.Message) error {
+	if msg == nil {
+		return errors.New("msg is nil")	
+	]
+	
+	if mc.client == nil {
+		return errors.New("nil client")
+	}
+
+	if !mc.client.IsConnectionOpen() {
+		return errors.New("connection is not active")	
+	}  
+	
+	rawData, err := translator.NewTransCoding().Encode(msg)
+	if err != nil {
+		return err
+	}
+
+	if token := mc.client.Publish(topic, qos, retained, rawData); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	return nil
+}
+
+//Subscribe
+// We Overide the function for the fn can process the model message.
+func (mc *MQTTClient) Subscribe(topic string, qos byte, fn func(msg *model.Message)) error {
+	if mc.client == nil {
+		return errors.New("nil client")
+	}
+
+	if !mc.client.IsConnectionOpen() {
+		return errors.New("connection is not active")	
+	}  
+
+	callback = func (client mqtt.Client, message mqtt.Message) {
+		msg := &model.Message{}
+		rawData := message.Payload()
+	
+		err := translator.NewTransCoding().Decode(rawData, msg)	
+		if err != nil {
+			klog.Infof("error message format, Ignored!")
+			return 
+		}
+
+		fn(msg)
+	}
+	if token := mc.client.Subscribe(topic, qos, callback); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	return nil
+}
+
+func (mc *MQTTClient) Unsubscribe(topics string) error {
+	if mc.client == nil {
+		return errors.New("nil client")
+	}
+
+	if !mc.client.IsConnectionOpen() {
+		return errors.New("connection is not active")	
+	}  
+
+	if token := mc.client.Unsubscribe(topics); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	return nil
+}
+
+func (mc *MQTTClient) Close() {
+	mc.client.Disconnect(250) 
 }
